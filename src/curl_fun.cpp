7795c032bbc6274dcbda759ca5ca0917903a78f9
Julien <butor@users.noreply.github.com>
2016-01-20 15:40:43 -0500

    Buffer displayed after a JSON error

--- src/curl_fun.cpp
+++ src/curl_fun.cpp
@@ -34,7 +34,10 @@ json_t* getJsonFromUrl(Parameters& params, std::string url, std::string postFiel
   root = json_loads(readBuffer.c_str(), 0, &error);
 
   while (!root) {
-    *params.logFile << "Error with JSON:\n" << error.text << ". Retrying..." << std::endl;
+    *params.logFile << "Error with JSON:\n" << error.text << std::endl;
+    *params.logFile << "Buffer:\n" << readBuffer.c_str() << std::endl;
+    *params.logFile << "Retrying..." << std::endl;
+    sleep(2.0);
     readBuffer = "";
     resCurl = curl_easy_perform(params.curl);
     while (resCurl != CURLE_OK) {
