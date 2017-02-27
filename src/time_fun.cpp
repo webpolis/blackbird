77ae173e6ae8a7eed808d69eba45e1e824c6fcf9
Greatwolf <github.greatwolf@mamber.net>
2017-02-26 22:03:37 -0800

    DRY'ed up the time functions.

--- src/time_fun.cpp
+++ src/time_fun.cpp
@@ -2,7 +2,8 @@
 #include <time.h>
 #include "time_fun.h"
 
-time_t getTime_t(int y, int m, int d, int h, int n, int s) {
+time_t getTime_t(int y, int m, int d, int h, int n, int s)
+{
   tm ttm = {0};
   ttm.tm_year = y - 1900;
   ttm.tm_mon = m - 1;
@@ -14,45 +15,33 @@ time_t getTime_t(int y, int m, int d, int h, int n, int s) {
   return mktime(&ttm);
 }
 
-std::string printDateTime(time_t t) {
-  struct tm timeinfo;
-  char buff[20];
-  strftime(buff, 20, "%m/%d/%Y %H:%M:%S", localtime_r(&t, &timeinfo));
-  std::string str(buff);
-  return str;
+template <const char *fmt>
+std::string fmtDateTime(const time_t &t)
+{
+  std::string buff(20, '\0');
+  buff.resize(strftime(&buff[0], buff.size(), fmt, localtime(&t)));
+  return buff;
 }
 
-std::string printDateTime() {
-  time_t now = time(NULL);
-  struct tm timeinfo;
-  char buff[20];
-  strftime(buff, 20, "%m/%d/%Y %H:%M:%S", localtime_r(&now, &timeinfo));
-  std::string str(buff);
-  return str;
+extern const char csvfmt[] = "%Y-%m-%d_%H:%M:%S";
+const decltype(&fmtDateTime<csvfmt>) printDateTimeCsv = &fmtDateTime<csvfmt>;
+
+extern const char dbfmt[] = "%Y-%m-%d %H:%M:%S";
+const decltype(&fmtDateTime<dbfmt>) printDateTimeDb = &fmtDateTime<dbfmt>;
+
+extern const char filenamefmt[] = "%Y%m%d_%H%M%S";
+std::string printDateTimeFileName()
+{
+  return fmtDateTime<filenamefmt>(time(NULL));
 }
 
-std::string printDateTimeCsv(time_t t) {
-  struct tm timeinfo;
-  char buff[20];
-  strftime(buff, 20, "%Y-%m-%d_%H:%M:%S", localtime_r(&t, &timeinfo));
-  std::string str(buff);
-  return str;
+extern const char defaultfmt[] = "%m/%d/%Y %H:%M:%S";
+std::string printDateTime(time_t t)
+{
+  return fmtDateTime<defaultfmt>(t);
 }
 
-std::string printDateTimeDb(time_t t) {
-  struct tm timeinfo;
-  char buff[20];
-  strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime_r(&t, &timeinfo));
-  std::string str(buff);
-  return str;
+std::string printDateTime()
+{
+  return printDateTime(time(NULL));
 }
-
-std::string printDateTimeFileName() {
-  time_t now = time(NULL);
-  struct tm timeinfo;
-  char buff[20];
-  strftime(buff, 20, "%Y%m%d_%H%M%S", localtime_r(&now, &timeinfo));
-  std::string str(buff);
-  return str;
-}
-
