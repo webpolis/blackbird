85efae7fd7f84e1368a2b0dce5045320d12a57ca
Greatwolf <github.greatwolf@mamber.net>
2017-03-11 22:18:48 -0800

    Allow specifying uppercase or lowercase.

--- src/hex_str.hpp
+++ src/hex_str.hpp
@@ -3,12 +3,20 @@
 
 #include <string>
 
-template <typename FwdIt>
+enum
+{
+  upperhex,
+  lowerhex
+};
+
+template <bool Caps = lowerhex, typename FwdIt>
 std::string hex_str(FwdIt first, FwdIt last)
 {
   static_assert(sizeof(typename std::iterator_traits<FwdIt>::value_type) == 1,
                 "value_type must be 1 byte.");
-  constexpr const char *bytemap = "0123456789abcdef";
+  constexpr const char *bytemap = Caps ?
+                                  "0123456789abcdef" :
+                                  "0123456789ABCDEF";
 
   std::string result(std::distance(first, last) * 2, '0');
 
