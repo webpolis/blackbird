28c5e3c18a467010d5dfed8c1e5734de9205d1d9
Greatwolf <github.greatwolf@mamber.net>
2017-05-08 15:21:06 -0700

    Refactored kraken to use restapi for auth endpoints. Removed unused headers.

--- src/exchange/kraken.cpp
+++ src/exchange/kraken.cpp
@@ -1,16 +1,13 @@
 #include "kraken.h"
 #include "parameters.h"
-#include "curl_fun.h"
 #include "utils/restapi.h"
 #include "utils/base64.h"
 
 #include "openssl/sha.h"
 #include "openssl/hmac.h"
 #include "jansson.h"
-#include <sstream>
 #include <iomanip>
 #include <vector>
-#include <unistd.h>
 #include <sys/time.h>
 
 namespace Kraken {
@@ -53,7 +50,7 @@ quote_t getQuote(Parameters &params)
 }
 
 double getAvail(Parameters& params, std::string currency) {
-  json_t* root = authRequest(params, "https://api.kraken.com", "/0/private/Balance");
+  json_t* root = authRequest(params, "/0/private/Balance");
   json_t* result = json_object_get(root, "result");
   if (json_object_size(result) == 0) {
     return 0.0;
@@ -85,7 +82,7 @@ std::string sendLongOrder(Parameters& params, std::string direction, double quan
   std::string pricelimit = std::to_string(price);
   std::string volume = std::to_string(quantity);
   std::string options = "pair=" + pair + "&type=" + type + "&ordertype=" + ordertype + "&price=" + pricelimit + "&volume=" + volume;
-  json_t* res = authRequest(params, "https://api.kraken.com", "/0/private/AddOrder", options);
+  json_t* res = authRequest(params, "/0/private/AddOrder", options);
   json_t* root = json_object_get(res, "result");
   if (json_is_object(root) == 0) {
     *params.logFile << json_dumps(res, 0) << std::endl;
@@ -98,7 +95,7 @@ std::string sendLongOrder(Parameters& params, std::string direction, double quan
 }
 
 bool isOrderComplete(Parameters& params, std::string orderId) {
-  json_t* root = authRequest(params, "https://api.kraken.com", "/0/private/OpenOrders");
+  json_t* root = authRequest(params, "/0/private/OpenOrders");
   // no open order: return true
   root = json_object_get(json_object_get(root, "result"), "open");
   if (json_object_size(root) == 0) {
@@ -148,81 +145,44 @@ double getLimitPrice(Parameters &params, double volume, bool isBid)
   return atof(json_string_value(json_array_get(json_array_get(branch, i-1), 0)));
 }
 
-json_t* authRequest(Parameters& params, std::string url, std::string request, std::string options) {
+json_t* authRequest(Parameters& params, std::string request, std::string options)
+{
+  using namespace std;
   // create nonce and POST data
   struct timeval tv;
   gettimeofday(&tv, NULL);
   unsigned long long nonce = (tv.tv_sec * 1000.0) + (tv.tv_usec * 0.001) + 0.5;
-  std::string post_data = "";
-  if (options.empty()) {
-    post_data = "nonce=" + std::to_string(nonce);
-  } else {
-    post_data = "nonce=" + std::to_string(nonce) + "&" + options;
-  }
+  string post_data = "nonce=" + to_string(nonce);
+  if (!options.empty())
+    post_data += "&" + options;
+
   // Message signature using HMAC-SHA512 of (URI path + SHA256(nonce + POST data))
   // and base64 decoded secret API key
-  std::string payload_for_signature = std::to_string(nonce) + post_data;
-  uint8_t digest[SHA256_DIGEST_LENGTH];
-  SHA256((uint8_t *)payload_for_signature.c_str(), payload_for_signature.size(), &digest[0]);
-  int signature_data_length = request.length() + SHA256_DIGEST_LENGTH;
-  std::vector<unsigned char> signature_data(signature_data_length);
-  auto signature_digest = std::copy(begin(request), end(request), begin(signature_data));
-  std::copy(std::begin(digest), std::end(digest), signature_digest);
-  std::string decoded_key = base64_decode(params.krakenSecret);
-  unsigned char* hmac_digest;
-  hmac_digest = HMAC(EVP_sha512(), decoded_key.c_str(), decoded_key.length(), signature_data.data(), signature_data.size(), NULL, NULL);
-  std::string api_sign_header = base64_encode(reinterpret_cast<const unsigned char*>(hmac_digest), SHA512_DIGEST_LENGTH);
+  auto sig_size = request.size() + SHA256_DIGEST_LENGTH;
+  vector<uint8_t> sig_data(sig_size);
+  copy(begin(request), end(request), begin(sig_data));
+
+  string payload_for_signature = to_string(nonce) + post_data;
+  SHA256((uint8_t *)payload_for_signature.c_str(), payload_for_signature.size(),
+         &sig_data[ sig_size - SHA256_DIGEST_LENGTH ]);
+
+  string decoded_key = base64_decode(params.krakenSecret);
+  uint8_t *hmac_digest = HMAC(EVP_sha512(),
+                              decoded_key.c_str(), decoded_key.length(),
+                              sig_data.data(), sig_data.size(), NULL, NULL);
+  string api_sign_header = base64_encode(hmac_digest, SHA512_DIGEST_LENGTH);
   // cURL header
-  struct curl_slist* headers = NULL;
-  std::string api = "API-KEY:" + std::string(params.krakenApi);
-  std::string api_sig = "API-Sign:" + api_sign_header;
-  headers = curl_slist_append(headers, api.c_str());
-  headers = curl_slist_append(headers, api_sig.c_str());
+  array<string, 2> headers
+  {
+    "API-KEY:"  + params.krakenApi,
+    "API-Sign:" + api_sign_header,
+  };
+
   // cURL request
-  CURLcode resCurl;
-  if (params.curl) {
-    std::string readBuffer;
-    curl_easy_setopt(params.curl, CURLOPT_POST, 1L);
-    curl_easy_setopt(params.curl, CURLOPT_HTTPHEADER, headers);
-    curl_easy_setopt(params.curl, CURLOPT_POSTFIELDS, post_data.c_str());
-    curl_easy_setopt(params.curl, CURLOPT_SSL_VERIFYPEER, 0L);
-    curl_easy_setopt(params.curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-    curl_easy_setopt(params.curl, CURLOPT_WRITEDATA, &readBuffer);
-    curl_easy_setopt(params.curl, CURLOPT_URL, (url+request).c_str());
-    curl_easy_setopt(params.curl, CURLOPT_CONNECTTIMEOUT, 10L);
-    resCurl = curl_easy_perform(params.curl);
-    json_t* root;
-    json_error_t error;
-    while (resCurl != CURLE_OK) {
-      *params.logFile << "<Kraken> Error with cURL. Retry in 2 sec...\n" << std::endl;
-      sleep(2.0);
-      readBuffer = "";
-      resCurl = curl_easy_perform(params.curl);
-    }
-    root = json_loads(readBuffer.c_str(), 0, &error);
-    while (!root) {
-      *params.logFile << "<Kraken> Error with JSON:\n" << error.text << std::endl;
-      *params.logFile << "<Kraken> Buffer:\n" << readBuffer.c_str() << std::endl;
-      *params.logFile << "<Kraken> Retrying..." << std::endl;
-      sleep(2.0);
-      readBuffer = "";
-      resCurl = curl_easy_perform(params.curl);
-      while (resCurl != CURLE_OK) {
-        *params.logFile << "<Kraken> Error with cURL. Retry in 2 sec...\n" << std::endl;
-        sleep(2.0);
-        readBuffer = "";
-        resCurl = curl_easy_perform(params.curl);
-      }
-      root = json_loads(readBuffer.c_str(), 0, &error);
-    }
-    curl_slist_free_all(headers);
-    curl_easy_reset(params.curl);
-    return root;
-  } else {
-    *params.logFile << "<Kraken> Error with cURL init." << std::endl;
-    return NULL;
-  }
-  return NULL;
+  auto &exchange = queryHandle(params);
+  return exchange.postRequest(request,
+                              make_slist(begin(headers), end(headers)),
+                              post_data);
 }
 
 }
