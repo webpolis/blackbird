95ac93ae01840355066269449eedb8f33faf1a95
Greatwolf <github.greatwolf@mamber.net>
2017-05-14 20:08:12 -0700

    RAII for jansson.

---
+++ src/unique_json.hpp
@@ -0,0 +1,18 @@
+#ifndef UNIQUE_JSON_HPP
+#define UNIQUE_JSON_HPP
+
+#include "jansson.h"
+#include <memory>
+
+struct json_deleter
+{
+  void operator () (json_t *J)
+  {
+    json_decref(J);
+    std::cerr << __PRETTY_FUNCTION__ << '\n';
+  }
+};
+
+using unique_json = std::unique_ptr<json_t, json_deleter>;
+
+#endif
