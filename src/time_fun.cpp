418f7869d588f9491e152dadad281c20dcf60f36
Greatwolf <github.greatwolf@mamber.net>
2017-06-10 04:39:51 -0700

    Using 'constexpr' in this fashion causes an internal compiler error in visual studio. Reverted back to 'extern const' as workaround.

--- src/time_fun.cpp
+++ src/time_fun.cpp
@@ -29,19 +29,19 @@ std::string fmtDateTime(const time_t &t) {
  * Also see SO question:
  *  http://stackoverflow.com/q/28835198/234175
  */
-static constexpr char csvfmt[] = "%Y-%m-%d_%H:%M:%S";
+extern const char csvfmt[] = "%Y-%m-%d_%H:%M:%S";
 const decltype(&fmtDateTime<csvfmt>) printDateTimeCsv = &fmtDateTime<csvfmt>;
 
-static constexpr char dbfmt[] = "%Y-%m-%d %H:%M:%S";
+extern const char dbfmt[] = "%Y-%m-%d %H:%M:%S";
 const decltype(&fmtDateTime<dbfmt>) printDateTimeDb = &fmtDateTime<dbfmt>;
 
-static constexpr char filenamefmt[] = "%Y%m%d_%H%M%S";
+extern const char filenamefmt[] = "%Y%m%d_%H%M%S";
 
 std::string printDateTimeFileName() {
   return fmtDateTime<filenamefmt>(time(NULL));
 }
 
-static constexpr char defaultfmt[] = "%m/%d/%Y %H:%M:%S";
+extern const char defaultfmt[] = "%m/%d/%Y %H:%M:%S";
 
 std::string printDateTime(time_t t) {
   return fmtDateTime<defaultfmt>(t);
