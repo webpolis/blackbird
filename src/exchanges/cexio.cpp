170cf88a2b3a94ec4ce51bf4f2cd8b49fe92700c
Christopher Ricciuti <christopherricciuti@christophersmbp.attlocal.net>
2018-02-02 11:57:38 -0800

    FInished optimization of Cex.io

--- src/exchanges/cexio.cpp
+++ src/exchanges/cexio.cpp
@@ -59,7 +59,7 @@ double getAvail(Parameters& params, std::string currency)
     
   unique_json root { authRequest(params, "/balance/","") };
 
-  const char * avail_str = json_string_value(json_object_get(root.get(), curr_));
+  const char * avail_str = json_string_value(json_object_get(json_object_get(root.get(), curr_), "available"));
   available = avail_str ? atof(avail_str) : 0.0;
 
   return available;
@@ -77,47 +77,53 @@ std::string sendShortOrder(Parameters& params, std::string direction, double qua
 
 std::string sendOrder(Parameters& params, std::string direction, double quantity, double price)
 {
-  *params.logFile << "<Cexio> Trying to send a \"" << direction << "\" limit order: "
-                  << std::setprecision(6) << quantity << "@$"
-                  << std::setprecision(2) << price << "...\n";
-  std::ostringstream oss;
-  oss << "\"type\":\"" << direction << "\", \"amount\":\"" << quantity << "\", \"price\":\"" << price;
-  std::string options = oss.str();
-  unique_json root { authRequest(params, "/place_order/BTC/USD", options) };
-  auto orderId = std::to_string(json_integer_value(json_object_get(root.get(), "order_id")));
-  *params.logFile << "<Cexio> Done (order ID: " << orderId << ")\n" << std::endl;
+  using namespace std;
+  string pair = "btc_usd";
+  string orderId = "";
+  *params.logFile << "<Cexio> Trying to send a " << pair << " " << direction << " limit order: " << quantity << "@" << price << endl;
+
+  ostringstream oss;
+  oss << "type=" << direction << "&amount=" << quantity << "&price=" << fixed << setprecision(2) << price;
+  string options = oss.str();
+
+  unique_json root { authRequest(params, "/place_order/BTC/USD/", options) };
+  auto error = json_string_value(json_object_get(root.get(), "error"));
+  if (error){
+    // auto dump = json_dumps(root.get(), 0);
+    // *params.logFile << "<Cexio> Error placing order: " << dump << ")\n" << endl;
+    // free(dump);
+    orderId = "0";
+  } else {
+    orderId = json_string_value(json_object_get(root.get(), "id"));
+    *params.logFile << "<Cexio> Done (order ID): " << orderId << ")\n" << endl;
+  }
   return orderId;
 }
 
 bool isOrderComplete(Parameters& params, std::string orderId)
 {
-  if (orderId == "0") return true;
+  using namespace std;
+  if (orderId == "0") return false;
 
-  auto options =  "\"order_id\":" + orderId;
-  unique_json root { authRequest(params, "/get_order", options) };
-  auto remains = json_integer_value(json_object_get(root.get(), "remains"));
-  if (remains==0){ // TO DO: Confirm that remains==0 means that order is complete
+  auto oId = stol(orderId);
+  ostringstream oss;
+  oss << "id=" << oId;
+  string options = oss.str();
+
+  unique_json root { authRequest(params, "/get_order/", options) };
+  auto remains = atof(json_string_value(json_object_get(root.get(), "remains")));
+  if (remains==0){
     return true;
-  } else { return false; }
+  } else { 
+    auto dump = json_dumps(root.get(), 0);
+    *params.logFile << "<Cexio> Order Not Complete: " << dump << ")\n" << endl;
+    free(dump);
+    // cout << "REMAINS:" << remains << endl;
+    return false; 
+    }
 }
 
-double getActivePos(Parameters& params) {
-  unique_json root { authRequest(params, "/open_positions/BTC/USD", "") };
-  double position;
-  if (json_array_size(root.get()) == 0)
-  {
-    *params.logFile << "<Cexio> WARNING: BTC position not available, return 0.0" << std::endl;
-    position = 0.0;
-  }
-  else
-  {
-    //TO DO: Make sure that this works. It probably doesn't.
-    position = atof(json_string_value(json_object_get(json_array_get(root.get(), 0), "data.amount")));
-  }
-  return position;
-  // double position = 1.0;
-  // return position;
-}
+double getActivePos(Parameters& params) { return getAvail(params, "btc"); }
 
 double getLimitPrice(Parameters &params, double volume, bool isBid)
 {
@@ -166,12 +172,7 @@ json_t* authRequest(Parameters &params, std::string request, std::string options
   }
 
   auto &exchange = queryHandle(params);
-  // *params.logFile << "<Cexio> PP: " << postParams << endl << endl;
-  // *params.logFile << "<Cexio> SIGN: " << headers[1] << endl << endl;
-  // *params.logFile << "<Cexio> NONCE: " << headers[2] << endl << endl;
   return checkResponse(*params.logFile, exchange.postRequest(request, postParams));
-  // return checkResponse(*params.logFile,exchange.postRequest(req, make_slist(begin(headers), end(headers))));
-
 }
 
 void testCexio() {
@@ -197,22 +198,37 @@ void testCexio() {
   cout << "Current bid limit price for 10 units: " << getLimitPrice(params, 10.0, true) << endl;
   cout << "Current ask limit price for 10 units: " << getLimitPrice(params, 10.0, false) << endl;
 
-  //cout << "Sending buy order - TXID: " ;
-  //orderId = sendLongOrder(params, "buy", 0.005, 1000);
-  //cout << orderId << endl;
+  // cout << "Sending buy order - TXID: " ;
+  // orderId = sendLongOrder(params, "buy", 0.002, 9510);
+  // cout << orderId << endl;
   // cout << "Buy order is complete: " << isOrderComplete(params, orderId) << endl;
+  // cout << "Active positions (BTC): " << getActivePos(params) << endl;
 
-  cout << "Active positions: " << getActivePos(params) << endl;
+  // cout << "Sending sell order - TXID: " ;
+  // orderId = sendLongOrder(params, "sell", 0.002, 8800);
+  // cout << orderId << endl;
+  // cout << "Sell order is complete: " << isOrderComplete(params, orderId) << endl;
 
-  //cout << "Sending sell order - TXID: " ;
-  //orderId = sendLongOrder(params, "sell", 0.5, 338);
-  //if (orderId == "0") {
-  //  cout << "failed" << endl;
-  //}
-  //else {
-  //  cout << orderId << endl;
-    // cout << "Sell order is complete: " << isOrderComplete(params, "404591373") << endl;
-  //}
+  // cout << "Active positions (BTC): " << getActivePos(params) << endl;
+
+  // cout << "Sending short order - TXID: " ;
+  // orderId = sendShortOrder(params, "sell", 0.002, 8800);
+  // cout << orderId << endl;
+  // cout << "Sell order is complete: " << isOrderComplete(params, orderId) << endl;
+
+  // cout << "Active positions: " << getActivePos(params) << endl;
+
+  /******* Cancel order functionality - for testing *******/
+  // long oId = 0; //Add orderId here
+  // ostringstream oss;
+  // oss << "id=" << oId;
+  // string options = oss.str();
+
+  // unique_json root { authRequest(params, "/cancel_order/", options) };
+  // auto dump = json_dumps(root.get(), 0);
+  // *params.logFile << "<Cexio> Error canceling order: " << dump << ")\n" << endl;
+  // free(dump);
+  /*********************************************************/
   }
 
 }
