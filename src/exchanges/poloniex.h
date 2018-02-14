2b72637b9ae50c7e3de9e5ef8c61ff641b62fb33
CCKRK <codykessler@gmail.com>
2018-02-14 10:26:20 -0600

    Poloniex integration

--- src/exchanges/poloniex.h
+++ src/exchanges/poloniex.h
@@ -18,7 +18,7 @@ std::string sendShortOrder(Parameters& params, std::string direction, double qua
 
 bool isOrderComplete(Parameters& params, std::string orderId);
 
-double getActivePos(Parameters& params);
+double getActivePos(Parameters& params, std::string orderId="");
 
 double getLimitPrice(Parameters& params, double volume, bool isBid);
 
